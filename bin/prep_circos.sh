c298fd5f6db15a3cdcff2e948a7dd3823cf87718
Barry Digby <b.digby237@gmail.com>
2021-02-03 10:03:45 +0000

    prep_circos.sh accounts for 'single exon' i.e ciRNA, EIciRNAs

--- bin/prep_circos.sh
+++ bin/prep_circos.sh
@@ -2,8 +2,19 @@
 
 file=$1
 
-awk '{print $11}' $file | tr ',' '\n' | awk 'NR>1{print buf}{buf=$0}' > exon_sizes
-seq $(awk '{print $10}' $file) > chrom
+## capture the exon sizes column, sep by comma if multiple
+awk '{print $11}' $file | tr ',' '\n' > exon_sizes_tmp
+## check number of exons by counting n lines
+cat exon_sizes_tmp
+len=$(wc -l exon_sizes_tmp | awk '{print $1}')
+## print buf removes empty last line inserted in process.
+## be careful here, do not execute this if only one line present (EIciRNA, ciRNA)
+#[[ $len -gt 1 ]] && awk 'NR>1{print buf}{buf=$0}' exon_sizes_tmp > exon_sizes || mv exon_sizes_tmp exon_sizes
+if [[ $len -gt 1 ]] ; then awk 'NR>1{print buf}{buf=$0}' exon_sizes_tmp > exon_sizes; else mv exon_sizes_tmp exon_sizes; fi
+## seq converts '2' to '1', '2'.. but does not work on just '1' (EIciRNA, ciRNA)
+n_exons=$(awk '{print $10}' $file)
+if [[ $n_exons -gt 1 ]]; then seq $n_exons > chrom; else printf $n_exons > chrom; fi
+#[[ $n_exons -gt 1]] && seq $n_exons > chrom || printf $n_exons > chrom
 paste chrom exon_sizes > tmp.txt
 
 rm -f exon_sizes
