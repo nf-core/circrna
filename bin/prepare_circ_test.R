acf79a2e5b9bcc56a3b1f509147b1695f3d771fd
Barry digby <b.digby237@gmail.com>
2022-10-08 21:09:35 +0100

    circRNA_matrix not count_matrix prepCLR.R

--- bin/prepare_circ_test.R
+++ bin/prepare_circ_test.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-circ_mat = read.table("count_matrix.txt", header=T, sep="\t", check.names = FALSE, stringsAsFactors = F, row.names = "ID")
+circ_mat = read.table("circRNA_matrix.txt", header=T, sep="\t", check.names = FALSE, stringsAsFactors = F, row.names = "ID")
 gene_mat = read.table("gene_count_matrix.csv", sep=",", header=T, row.names="gene_id", stringsAsFactors = F)
 map = read.table("circrna_host-gene.txt", header = F, sep="\t", stringsAsFactors = F)
 
