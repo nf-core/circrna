2a6541c464bd90093b3537ed36cce721a9301b89
Alexander Peltzer <alex.peltzer@gmail.com>
2022-10-12 09:13:10 +0000

    Fix editorconfig

--- bin/backsplice_gen.sh
+++ bin/backsplice_gen.sh
@@ -7,18 +7,18 @@ file_prefix=$(basename $1 .fa)
 # Split multi-fasta file into single records
 mkdir canonical_seqs && mv $1 canonical_seqs/
 ls -d canonical_seqs/*.fa | while read -r line; do
-  cat $line | awk '{if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fa")} print $0 > filename}'
+cat $line | awk '{if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fa")} print $0 > filename}'
 done
 
 # add backsplice site to individual fasta files
 for file in *.fa; do
-  fn=$(basename $file .fa)
-  cat $file | grep ">" > header.txt
-  cat $file | grep -v ">" | cut -c1-20 > 20bp.txt
-  cat $file | grep -v ">" > seqs.txt
-  paste seqs.txt 20bp.txt | sed -e 's/\t//' > seqs_20bp.txt
-  paste header.txt seqs_20bp.txt | sed -e 's/\t/\n/' > ${fn}.fasta
-  rm header.txt 20bp.txt seqs.txt seqs_20bp.txt
+    fn=$(basename $file .fa)
+    cat $file | grep ">" > header.txt
+    cat $file | grep -v ">" | cut -c1-20 > 20bp.txt
+    cat $file | grep -v ">" > seqs.txt
+    paste seqs.txt 20bp.txt | sed -e 's/\t//' > seqs_20bp.txt
+    paste header.txt seqs_20bp.txt | sed -e 's/\t/\n/' > ${fn}.fasta
+    rm header.txt 20bp.txt seqs.txt seqs_20bp.txt
 done
 
 # rm single fasta entries from while read line
@@ -29,5 +29,5 @@ cat *.fasta > ${file_prefix}.fa
 rm *.fasta
 # move canonical seqs back to publish to outdir
 ls -d canonical_seqs/*.fa | while read -r line; do
-  mv $line ${file_prefix}.fasta
+mv $line ${file_prefix}.fasta
 done
