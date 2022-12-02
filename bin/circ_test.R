9564bd792d4e082fe8591a849f3838744a093cb8
Barry digby <b.digby237@gmail.com>
2022-12-02 21:28:06 +0000

    indentation

--- bin/circ_test.R
+++ bin/circ_test.R
@@ -17,39 +17,40 @@ require(ggplot2)
 ##   conf.interval: the percent range of the confidence interval (default is 95%)
 #'
 summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
-                      conf.interval=.95, .drop=TRUE) {
-  require(plyr)
+                        conf.interval=.95, .drop=TRUE) {
 
-  # New version of length which can handle NA's: if na.rm==T, don't count them
-  length2 <- function (x, na.rm=FALSE) {
-    if (na.rm) sum(!is.na(x))
-    else       length(x)
-  }
+    require(plyr)
 
-  # This does the summary. For each group's data frame, return a vector with
-  # N, mean, and sd
-  datac <- ddply(data, groupvars, .drop=.drop,
-                .fun = function(xx, col) {
-                    c(N    = length2(xx[[col]], na.rm=na.rm),
-                    mean = mean   (xx[[col]], na.rm=na.rm),
-                    sd   = sd     (xx[[col]], na.rm=na.rm)
-                )
-                },
-                measurevar
-  )
+    # New version of length which can handle NA's: if na.rm==T, don't count them
+    length2 <- function (x, na.rm=FALSE) {
+        if (na.rm) sum(!is.na(x))
+        else       length(x)
+    }
 
-  # Rename the "mean" column
-  datac <- rename(datac, c("mean" = measurevar))
+    # This does the summary. For each group's data frame, return a vector with
+    # N, mean, and sd
+    datac <- ddply(data, groupvars, .drop=.drop,
+                    .fun = function(xx, col) {
+                        c(N    = length2(xx[[col]], na.rm=na.rm),
+                        mean = mean   (xx[[col]], na.rm=na.rm),
+                        sd   = sd     (xx[[col]], na.rm=na.rm)
+                    )
+                    },
+                    measurevar
+    )
 
-  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+    # Rename the "mean" column
+    datac <- rename(datac, c("mean" = measurevar))
 
-  # Confidence interval multiplier for standard error
-  # Calculate t-statistic for confidence interval:
-  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
-  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
-  datac$ci <- datac$se * ciMult
+    datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
 
-  return(datac)
+    # Confidence interval multiplier for standard error
+    # Calculate t-statistic for confidence interval:
+    # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+    ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+    datac$ci <- datac$se * ciMult
+
+    return(datac)
 }
 
 ## RATIO PLOT
