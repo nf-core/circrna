0200a43828ad59bd20a9c126f44ca7505b16e978
Malte Weyrich <malte.weyrich@hotmail.de>
2024-08-28 18:02:56 +0200

    adding sponge for testing

---
+++ modules/local/sponge/main.nf
@@ -0,0 +1,44 @@
+process SPONGE {
+    tag "$meta.id"
+    label 'process_high'
+
+    conda "${moduleDir}/environment.yml"
+    container "${ workflow.containerEngine == 'singularity' && !task.ext.singularity_pull_docker_container ?
+        'oras://community.wave.seqera.io/library/bioconductor-deseq2_bioconductor-sponge_r-doparallel_r-foreach_r-visnetwork:379bbce95f2c4913' :
+        'community.wave.seqera.io/library/bioconductor-deseq2_bioconductor-sponge_r-doparallel_r-foreach_r-visnetwork:f38bbe7441604bcc' }"
+
+    input:
+    tuple val(meta), path(binding_sites)
+    tuple val(meta), path(gene_expr)
+    tuple val(meta), path(mirna_expr)
+
+
+    output:
+    path "versions.yml"                             , emit: versions
+    path "sponge.RData"                             , emit: sponge_data
+    path "circRNAs_as_ceRNAs.tsv"                   , emit: cernas
+    path "total_plots.pdf"                          , emit: total_plots
+    path "simulation_plots.pdf"                     , emit: simulation_plots
+    path "circRNA_plots.pdf"                        , emit: circ_plots
+
+
+    when:
+    task.ext.when == null || task.ext.when
+
+    script:
+    template 'sponge.R'
+
+    stub:
+    """
+    touch "sponge.RData"
+
+    cat <<-END_VERSIONS > versions.yml
+    "${task.process}":
+        bioconductor-deseq2: \$(Rscript -e "library(DESeq2); cat(as.character(packageVersion('DESeq2')))")
+        bioconductor-sponge: \$(Rscript -e "library(SPONGE); cat(as.character(packageVersion('SPONGE')))")
+        r-visnetwork: \$(Rscript -e "library(visNetwork); cat(as.character(packageVersion('visNetwork')))")
+        r-doparallel: \$(Rscript -e "library(doParallel); cat(as.character(packageVersion('doParallel')))")
+        r-foreach: \$(Rscript -e "library(foreach); cat(as.character(packageVersion('foreach')))")
+    END_VERSIONS
+    """
+}
---
+++ modules/local/sponge/templates/sponge.R
@@ -0,0 +1,212 @@
+#!/usr/bin/env Rscript
+
+library(SPONGE)
+library(visNetwork)
+library(doParallel)
+library(foreach)
+library(DESeq2)
+
+circ_mRNA_subnetwork <- function(interactions, pattern) {
+    return(interactions[
+        (grepl(pattern, interactions\$geneA) & !grepl(pattern, interactions\$geneB)) |
+        (grepl(pattern, interactions\$geneB) & !grepl(pattern, interactions\$geneA)),
+    ])
+}
+
+normalize_data <- function(data){
+    samples <- colnames(data)
+    meta <- data.frame(row.names = samples)
+    data <- as.matrix(data)
+    dds <- DESeq2::DESeqDataSetFromMatrix(countData = round(data + 1), colData = meta, design = ~ 1)
+    dds <- DESeq2::estimateSizeFactors(dds)
+    return(DESeq2::counts(dds, normalized=TRUE))
+}
+
+# load binding sites matrix
+bindingsites <- read.table("${binding_sites}", sep = "\\t", header = TRUE, check.names = FALSE)
+rownames(bindingsites) <- bindingsites[, 1]
+bindingsites <- bindingsites[, -1]
+bindingsites_matrix <- as.matrix(bindingsites)
+
+
+# load gene expression
+genes <- read.csv("${gene_expr}", sep = "\\t", row.names = "tx")
+genes <- subset(genes, select = -gene_id)
+# prep for DESeq2
+gene_expr <- normalize_data(as.matrix(genes))
+gene_expr <- t(as.matrix(genes))
+
+
+# load mirna expression
+mirna <- read.csv("${mirna_expr}", sep = "\\t", row.names = "miRNA")
+mirna[is.na(mirna)] <- 0
+mir_expr <- t(as.matrix(mirna))
+
+
+# ensure that both matrices have the same samples
+common_samples <- intersect(rownames(gene_expr), rownames(mir_expr))
+
+# subset and reorder the rows (samples) of both matrices to match each other (as specified in SPONGE docs)
+gene_expr <- gene_expr[common_samples, ]
+mir_expr <- mir_expr[common_samples, ]
+
+
+######################################
+#                A                   #
+######################################
+logging.file <- "sponge.log"
+# number of cores from args
+cores <- ${params.sponge_cpus}
+
+cl <- makeCluster(cores, outfile=logging.file)
+registerDoParallel(cl)
+
+gene_expriRNA_candidates <- SPONGE::sponge_gene_miRNA_interaction_filter(
+    gene_expr = gene_expr,
+    mir_expr = mir_expr,
+    mir_predicted_targets = bindingsites_matrix,
+    F.test = FALSE,
+    coefficient.threshold = -0.01,
+    coefficient.direction = NULL
+)
+
+
+    # F.test.p.adj.threshold = ${params.sponge_f_test_pval},
+    # coefficient.threshold = ${params.sponge_coeff_threshold},
+    # elastic.net = ("${params.sponge_elastic_net}" == "true")
+
+######################################
+#                B                   #
+######################################
+ceRNA_interactions <- SPONGE::sponge(
+    gene_expr = gene_expr,
+    mir_expr = mir_expr,
+    mir_interactions = gene_expriRNA_candidates
+)
+
+
+######################################
+#                C                   #
+######################################
+
+mscor_null_model <- SPONGE::sponge_build_null_model(
+    number_of_datasets = 100,
+    number_of_samples = nrow(gene_expr)
+)
+
+sim_plot <- SPONGE::sponge_plot_simulation_results(mscor_null_model)
+pdf("simulation_plots.pdf")
+plot(sim_plot)
+dev.off()
+
+ceRNA_interactions_sign <- SPONGE::sponge_compute_p_values(
+    sponge_result = ceRNA_interactions,
+    null_model = mscor_null_model
+)
+
+######################################
+#                D                   #
+######################################
+ceRNA_interactions_fdr <- ceRNA_interactions_sign[which(ceRNA_interactions_sign\$p.adj < ${params.sponge_fdr}), ]
+min.interactions <- 5000
+
+if (nrow(ceRNA_interactions_fdr) < min.interactions && nrow(ceRNA_interactions_sign) > min.interactions) {
+    print("Warning: fdr setting too strict, no significant interactions detected; min of padj is:")
+    print(min(ceRNA_interactions_sign\$p.adj))
+    print("adjusting...")
+    fdr <- min(ceRNA_interactions_sign\$p.adj) * 1.01
+    ceRNA_interactions_fdr <- ceRNA_interactions_sign[which(ceRNA_interactions_sign\$p.adj < fdr), ]
+    while(nrow(ceRNA_interactions_fdr) < min.interactions) {
+        fdr <- fdr * 1.01
+        ceRNA_interactions_fdr <- ceRNA_interactions_sign[which(ceRNA_interactions_sign\$p.adj < fdr), ]
+    }
+    cat("adjusted fdr to :", fdr, "to allow for a minimum", min.interactions, "interactions", "\\n")
+    cat("current fdr relevant interactions:", nrow(ceRNA_interactions_fdr))
+    ceRNA_interactions_fdr <- ceRNA_interactions_fdr[order(ceRNA_interactions_fdr\$p.adj), ]
+}
+
+# MOST SIGNIFICANT SPONGES
+network_centralities <- SPONGE::sponge_node_centralities(ceRNA_interactions_fdr)
+ceRNA_interactions_weight <- ceRNA_interactions_fdr
+ceRNA_interactions_weight\$weight <- -log10(ceRNA_interactions_fdr\$p.adj)
+weighted_network_centralities <- SPONGE::sponge_node_centralities(ceRNA_interactions_weight)
+weighed_network_plot <- SPONGE::sponge_plot_network_centralities(weighted_network_centralities, top = 3)
+pdf("total_plots.pdf")
+plot(weighed_network_plot)
+dev.off()
+
+# CIRC RNA MRNA ONLY
+circ_mRNA_only <- circ_mRNA_subnetwork(ceRNA_interactions_fdr, "circ")
+write.table(circ_mRNA_only, "circRNAs_as_ceRNAs.tsv", sep = "\\t", row.names = FALSE)
+
+# TODO: REMOVE
+write.table(ceRNA_interactions, "ceRNAs_interactions.tsv", sep = "\\t", row.names = FALSE)
+
+# NETWORK ANALYSIS CIRC
+ceRNA_interactions_circ_weight <- circ_mRNA_only
+ceRNA_interactions_circ_weight\$weight <- -log10(circ_mRNA_only\$p.val)
+weighted_network_centralities_circ <- SPONGE::sponge_node_centralities(ceRNA_interactions_circ_weight)
+
+# plot top n samples
+n = 10 # TODO: param
+
+# betweeness
+top_network_plot_btw <- SPONGE::sponge_plot_network_centralities(
+    weighted_network_centralities_circ,
+    top = n,
+    measure = "btw"
+)
+
+# eigenvector
+top_network_plot_ev <- SPONGE::sponge_plot_network_centralities(
+    weighted_network_centralities_circ,
+    top = n,
+    measure = "ev"
+)
+
+# counts
+top_network_plot_c <- SPONGE::sponge_plot_network_centralities(
+    weighted_network_centralities_circ,
+    top = n,
+    measure = "count"
+)
+
+pdf("circRNA_plots.pdf")
+plot(top_network_plot_btw)
+plot(top_network_plot_ev)
+plot(top_network_plot_c)
+dev.off()
+stopCluster(cl)
+
+# save R objects for sponge effects
+save.image(file =  "sponge.RData")
+
+################################################
+################################################
+## VERSIONS FILE                              ##
+################################################
+################################################
+
+deseq2.version <- as.character(packageVersion('DESeq2'))
+sponge.version <- as.character(packageVersion('SPONGE'))
+visnetwork.version <- as.character(packageVersion('visNetwork'))
+doparallel.version <- as.character(packageVersion('doParallel'))
+foreach.version <- as.character(packageVersion('foreach'))
+
+writeLines(
+    c(
+        '"${task.process}":',
+        paste('    bioconductor-deseq2:', deseq2.version),
+        paste('    bioconductor-sponge:', sponge.version),
+        paste('    r-visnetwork:', visnetwork.version),
+        paste('    r-doparallel:', doparallel.version),
+        paste('    r-foreach:', foreach.version)
+    ),
+'versions.yml')
+
+
+################################################
+################################################
+################################################
+################################################
+################################################
