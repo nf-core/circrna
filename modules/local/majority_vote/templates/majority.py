2075095f035f9b6a1992f046c0dbb6f203c9fa32
Nico Trummer <nictru32@gmail.com>
2025-01-16 23:36:24 +0100

    Improve mirna majority vote script

--- modules/local/majority_vote/templates/majority.py
+++ modules/local/majority_vote/templates/majority.py
@@ -3,39 +3,23 @@ import platform
 import polars as pl
 import yaml
 
-# paths = "${bindingsites}".split(" ")
-paths = []
-bindingsites = "${bindingsites}"
+df = pl.scan_csv("*.tsv",
+                separator="\\t",
+                has_header=False,
+                new_columns=['mirna', 'target', 'start', 'end', 'tool'])
 
-j = 0
-for i in range(len(bindingsites)):
-    if bindingsites[i] == " " or i == len(bindingsites) - 1:
-        paths.append(bindingsites[j : i + 1])
-        j = i + 1
+df = df.select("mirna", "target", "tool")
+df = df.group_by('mirna', 'target').agg(pl.col("tool").n_unique())
 
-print(paths[1:3])
-print(len(paths))
-
-df = pl.scan_csv(paths,
-                 separator="\\t",
-                 has_header=False,
-                 new_columns=['mirna', 'target', 'start', 'end', 'tool'])
-
-df = df.select(["mirna", "target", "tool"])
-
-df = df.group_by(['mirna', 'target']).agg(pl.col("tool").n_unique())
-
-df = df.filter(pl.col("tool") >= int("${min_tools}")) \
-    .select(["mirna", "target"])
+df = df.filter(pl.col("tool") >= int("${min_tools}"))
+df = df.select("mirna", "target")
 
 df = df.collect()
-
 df.write_csv('${meta.id}.majority.tsv', separator='\\t', include_header=False)
 
 # Create targets file
 
 df = df.group_by('mirna').agg(pl.col("target").str.concat(","))
-
 df.write_csv('${meta.id}.targets.tsv', separator='\\t', include_header=False)
 
 # Create version file
